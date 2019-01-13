bbf0fd3e6ce8203d731d30c93cd46008dbe5ee78
Yarsan Hoessain <32988993+hyarsan@users.noreply.github.com>
2019-01-13 20:32:08 +0100

    cargo fmt it

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use console::{Term}; 
+use console::Term;
 
 use chrono::{Datelike, Timelike, Utc, Weekday};
 
-use std::io::{Error};
+use std::io::Error;
 use std::{thread, time};
 
 const WHALE: &str = "
@@ -33,41 +33,34 @@ fn main() -> Result<(), Error> {
 
         for (i, line) in WHALE.split('\n').enumerate() {
             match i {
-                0 ... 6 => term.write_line(line),
-                7 => {
-                    term.write_line(
-                        &format!("{}Date: {month} {day:02} , {year}", line,
-                            month = get_month_name(now.month()),
-                            day = now.day(),
-                            year = now.year(),
-                        )
-                    )
-                },
-                8 => {
-                    term.write_line(
-                        &format!("{}Weekday: {weekday}", line,
-                            weekday = get_weekday_name(now.weekday())
-                        )
-                    )
-                },
-                9 => {
-                    term.write_line(
-                        &format!("{}Time: {hour:02} : {minute:02} : {second:02}", line,
-                            hour = now.hour() + 1,
-                            minute = now.minute(),
-                            second = now.second(),
-                        )
-                    )
-                },
-                10 => {
-                    term.write_line(
-                        &format!("{}Timezone: {timezone}", line,
-                            timezone = now.timezone(),
-                        )
-                    )
-                },
+                0...6 => term.write_line(line),
+                7 => term.write_line(&format!(
+                    "{}Date: {month} {day:02} , {year}",
+                    line,
+                    month = get_month_name(now.month()),
+                    day = now.day(),
+                    year = now.year(),
+                )),
+                8 => term.write_line(&format!(
+                    "{}Weekday: {weekday}",
+                    line,
+                    weekday = get_weekday_name(now.weekday())
+                )),
+                9 => term.write_line(&format!(
+                    "{}Time: {hour:02} : {minute:02} : {second:02}",
+                    line,
+                    hour = now.hour() + 1,
+                    minute = now.minute(),
+                    second = now.second(),
+                )),
+                10 => term.write_line(&format!(
+                    "{}Timezone: {timezone}",
+                    line,
+                    timezone = now.timezone(),
+                )),
                 _ => term.write_line(line),
-            }.expect("could not write line");
+            }
+            .expect("could not write line");
         }
 
         thread::sleep(time::Duration::from_secs(1));
@@ -91,7 +84,7 @@ fn get_month_name(month: u32) -> &'static str {
         10 => "October",
         11 => "November",
         12 => "December",
-        _  => unreachable!(),
+        _ => unreachable!(),
     }
 }
 
